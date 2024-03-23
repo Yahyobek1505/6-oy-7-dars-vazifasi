a53c2aa5fd174c0d729f44114675460d9e35bcdf
Yahyobek1505 <yahyohonorinboyev0105@gmail.com>
2024-03-23 20:19:04 +0500

    Homework is done !

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,22 +1,21 @@
-import React from "react";
 import logo from "../../assets/Logo.png";
-import "./index.css";
+import styles from "./index.module.css"
 function Header() {
   return (
     <>
-      <header className="header">
-        <div className="imgLogo">
+      <header className={styles.header}>
+        <div className={styles.imgLogo}>
           <img src={logo} alt="" />
         </div>
-        <div className="nav">
-        <ul className="menu">
+        <div className={styles.nav}>
+        <ul className={styles.menu}>
           <li>Home</li>
           <li>About us</li>
           <li>How it works</li>
           <li>Services</li>
           <li>Contact</li>
         </ul>
-        <button className="btn">Get Started</button>
+        <button className={styles.btn}>Get Started</button>
         </div>
       </header>
     </>
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import "./index.css"
+import style from './index.module.css'
 import img from '../../assets/macbook.png'
 import icon from '../../assets/icon.png'
 function Main() {
   return (
     <>
-      <div className="main">
-      <h1 className='title'>Launch your own online learning platform</h1>
-      <p className='paragrph'>Top instructors from around the world teach millions of students on kreed. Get unlimited access to 2,000+ courses for your team.</p>
-      <button className='button'>Explore Courses</button>
+      <div className={style.main}>
+      <h1 className={style.title}>Launch your own online learning platform</h1>
+      <p className={style.paragrph}>Top instructors from around the world teach millions of students on kreed. Get unlimited access to 2,000+ courses for your team.</p>
+      <button className={style.button}>Explore Courses</button>
       </div>
-      <div className="images">
-      <img className='img' src={img} alt="" />
-      <img className='icon' src={icon} alt="" />
+      <div className={style.images}>
+      <img className={style.img} src={img} alt="" />
+      <img className={style.icon} src={icon} alt="" />
       </div>
     </>
   )
